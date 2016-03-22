d9f83cfe2a4aaeaa6185b054aaaddbca676dacd2
UCodeUStory <qiyuekoon@foxmail.com>
2016-03-22 18:28:50 +0800

    add SwipeLayout
    add SwipeLayout

---
+++ app/src/main/java/com/example/swipe/SwipeLayout.java
@@ -0,0 +1,26 @@
+package com.example.swipe;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+ /**
+ * @ Author: qiyue (ustory)
+ * @ Email: qiyuekoon@foxmail.com
+ * @ Data:2016/3/22
+ */
+public class SwipeLayout extends FrameLayout {
+    public SwipeLayout(Context context) {
+        super(context);
+    }
+
+    public SwipeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SwipeLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
